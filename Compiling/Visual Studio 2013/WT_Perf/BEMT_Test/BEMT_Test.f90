85f34670e941d9674f92a58ec86cb206f5a4156a
Greg Hayman <gregory.hayman@nrel.gov>
2015-05-26 14:56:14 +0000

    Committing WT_Perf driver project
    git-svn-id: https://windsvn2.nrel.gov/WT_Perf/branches/v4.x@101 a6739609-9c9d-4a29-a9ec-bdd8708b3f03

---
+++ Compiling/Visual Studio 2013/WT_Perf/BEMT_Test/BEMT_Test.f90
@@ -0,0 +1,25 @@
+!  BEMT_Test.f90 
+!
+!  FUNCTIONS:
+!  BEMT_Test - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: BEMT_Test
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program BEMT_Test
+
+    implicit none
+
+    ! Variables
+
+    ! Body of BEMT_Test
+    print *, 'Hello World'
+
+    end program BEMT_Test
+
---
+++ Compiling/Visual Studio 2013/WT_Perf/RunRegistry.bat
@@ -0,0 +1,89 @@
+@ECHO OFF
+
+set lines=======================================================================
+echo %lines%
+IF "%1"=="" (
+ECHO.
+ECHO   The calling syntax for this script is
+ECHO             RunRegistry ModuleName
+ECHO.
+GOTO Done
+)
+
+
+REM ----------------------------------------------------------------------------
+REM ------------------------- LOCAL PATHS --------------------------------------
+REM ----------------------------------------------------------------------------
+REM -- USERS MAY EDIT THESE PATHS TO POINT TO FOLDERS ON THEIR LOCAL MACHINES. -
+REM -- NOTE: do not use quotation marks around the path names!!!! --------------
+REM ----------------------------------------------------------------------------
+REM ----------------------------------------------------------------------------
+
+SET Registry=..\..\..\bin\Registry_win32.exe
+SET Source_Loc=..\..\..\Source
+
+SET NWTC_Lib_Loc=%Source_Loc%\dependencies\NWTC_Library
+SET AirfoilInfo_Loc=%Source_Loc%\dependencies\Airfoil_Info
+SET BEMT_Loc=%Source_Loc%\dependencies\BEMT
+SET UnsteadyAero_Loc=%Source_Loc%\dependencies\UnsteadyAero
+
+
+
+IF /I "%2"=="bjonkman" CALL ..\Set_FAST_paths.bat
+
+SET ModuleName=%1
+
+IF "%ModuleName%"=="BEMT" GOTO BEMT
+IF "%ModuleName%"=="AirfoilInfo" GOTO AFI
+IF "%ModuleName%"=="AeroDyn" GOTO AeroDyn
+REM ----------------------------------------------------------------------------
+REM ---------------- RUN THE REGISTRY TO AUTO-GENERATE FILES -------------------
+REM ----------------------------------------------------------------------------
+
+:UnsteadyAero
+SET CURR_LOC=%Source_Loc%\dependencies\UnsteadyAero
+%REGISTRY% "%CURR_LOC%\UnsteadyAero_Registry.txt" -I %NWTC_Lib_Loc% -I %AirfoilInfo_Loc% -O %CURR_LOC%
+GOTO checkError
+
+:BEMT
+SET CURR_LOC=%Source_Loc%\dependencies\BEMT
+%REGISTRY% "%CURR_LOC%\BEMT_Registry.txt" -I %NWTC_Lib_Loc% -I %AirfoilInfo_Loc% -I %UnsteadyAero_Loc% -O %CURR_LOC%
+GOTO checkError
+
+:AeroDyn
+SET CURR_LOC=%Source_Loc%\dependencies\AeroDyn
+%REGISTRY% "%CURR_LOC%\AeroDyn_Registry.txt" -I %NWTC_Lib_Loc% -I %AirfoilInfo_Loc% -I %BEMT_Loc% -I %UnsteadyAero_Loc% -O %CURR_LOC%
+GOTO checkError
+
+:AFI
+SET CURR_LOC=%Source_Loc%\dependencies\Airfoil_Info
+%REGISTRY% "%CURR_LOC%\AirfoilInfo_Registry.txt" -I %NWTC_Lib_Loc% -O %CURR_LOC% -noextrap
+GOTO checkError
+
+:checkError
+ECHO.
+IF %ERRORLEVEL% NEQ 0 (
+ECHO Error running  Registry for %ModuleName%.
+) ELSE (
+ECHO %ModuleName%_Types.f90 was created.
+)
+
+
+
+
+:end
+REM ----------------------------------------------------------------------------
+REM ------------------------- CLEAR MEMORY -------------------------------------
+REM ----------------------------------------------------------------------------
+ECHO. 
+
+
+SET REGISTRY=
+
+SET NWTC_Lib_Loc=
+SET Source_Loc=
+
+SET CURR_LOC=
+:Done
+echo %lines%
+set lines=
