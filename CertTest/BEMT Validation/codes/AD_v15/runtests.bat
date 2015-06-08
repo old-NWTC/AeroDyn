d337a0a23119344351c9e21ed3664698eef6a89b
Greg Hayman <gregory.hayman@nrel.gov>
2015-06-08 22:49:02 +0000

    Updated script to move results files and updated binary
    git-svn-id: https://windsvn2.nrel.gov/WT_Perf/branches/v4.x@120 a6739609-9c9d-4a29-a9ec-bdd8708b3f03

--- CertTest/BEMT Validation/codes/AD_v15/runtests.bat
+++ CertTest/BEMT Validation/codes/AD_v15/runtests.bat
@@ -1,9 +1,43 @@
 cd "test cases"
 ..\bin\AeroDyn_Driver.exe skewWakeCorr.dvr
-rem move skewWakeCorr.WTP.out ..\..\..\data\AD_v15\skewWakeCorr.out
+move skewWakeCorr.1.out ..\..\..\data\AD_v15\skewWakeCorr.1.out
+move skewWakeCorr.2.out ..\..\..\data\AD_v15\skewWakeCorr.2.out
+move skewWakeCorr.3.out ..\..\..\data\AD_v15\skewWakeCorr.3.out
+move skewWakeCorr.4.out ..\..\..\data\AD_v15\skewWakeCorr.4.out
+move skewWakeCorr.5.out ..\..\..\data\AD_v15\skewWakeCorr.5.out
+move skewWakeCorr.6.out ..\..\..\data\AD_v15\skewWakeCorr.6.out
+move skewWakeCorr.7.out ..\..\..\data\AD_v15\skewWakeCorr.7.out
+move skewWakeCorr.8.out ..\..\..\data\AD_v15\skewWakeCorr.8.out
+move skewWakeCorr.9.out ..\..\..\data\AD_v15\skewWakeCorr.9.out
+move skewWakeCorr.10.out ..\..\..\data\AD_v15\skewWakeCorr.10.out
+move skewWakeCorr.11.out ..\..\..\data\AD_v15\skewWakeCorr.11.out
+move skewWakeCorr.12.out ..\..\..\data\AD_v15\skewWakeCorr.12.out
+move skewWakeCorr.13.out ..\..\..\data\AD_v15\skewWakeCorr.13.out
+move skewWakeCorr.14.out ..\..\..\data\AD_v15\skewWakeCorr.14.out
+move skewWakeCorr.15.out ..\..\..\data\AD_v15\skewWakeCorr.15.out
+move skewWakeCorr.16.out ..\..\..\data\AD_v15\skewWakeCorr.16.out
+move skewWakeCorr.17.out ..\..\..\data\AD_v15\skewWakeCorr.17.out
+move skewWakeCorr.18.out ..\..\..\data\AD_v15\skewWakeCorr.18.out
 
 ..\bin\AeroDyn_Driver.exe noSkewWakeCorr.dvr
-rem move noSkewWakeCorr.WTP.out ..\..\..\data\AD_v15\noSkewWakeCorr.out
+move noSkewWakeCorr.1.out ..\..\..\data\AD_v15\noSkewWakeCorr.1.out
+move noSkewWakeCorr.2.out ..\..\..\data\AD_v15\noSkewWakeCorr.2.out
+move noSkewWakeCorr.3.out ..\..\..\data\AD_v15\noSkewWakeCorr.3.out
+move noSkewWakeCorr.4.out ..\..\..\data\AD_v15\noSkewWakeCorr.4.out
+move noSkewWakeCorr.5.out ..\..\..\data\AD_v15\noSkewWakeCorr.5.out
+move noSkewWakeCorr.6.out ..\..\..\data\AD_v15\noSkewWakeCorr.6.out
+move noSkewWakeCorr.7.out ..\..\..\data\AD_v15\noSkewWakeCorr.7.out
+move noSkewWakeCorr.8.out ..\..\..\data\AD_v15\noSkewWakeCorr.8.out
+move noSkewWakeCorr.9.out ..\..\..\data\AD_v15\noSkewWakeCorr.9.out
+move noSkewWakeCorr.10.out ..\..\..\data\AD_v15\noSkewWakeCorr.10.out
+move noSkewWakeCorr.11.out ..\..\..\data\AD_v15\noSkewWakeCorr.11.out
+move noSkewWakeCorr.12.out ..\..\..\data\AD_v15\noSkewWakeCorr.12.out
+move noSkewWakeCorr.13.out ..\..\..\data\AD_v15\noSkewWakeCorr.13.out
+move noSkewWakeCorr.14.out ..\..\..\data\AD_v15\noSkewWakeCorr.14.out
+move noSkewWakeCorr.15.out ..\..\..\data\AD_v15\noSkewWakeCorr.15.out
+move noSkewWakeCorr.16.out ..\..\..\data\AD_v15\noSkewWakeCorr.16.out
+move noSkewWakeCorr.17.out ..\..\..\data\AD_v15\noSkewWakeCorr.17.out
+move noSkewWakeCorr.18.out ..\..\..\data\AD_v15\noSkewWakeCorr.18.out
 
 
 
