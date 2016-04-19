14eb058d9dd5b2178418a75a5eee74af5b854cdd
Greg Hayman <gregory.hayman@nrel.gov>
2016-04-19 14:55:21 +0000

    Updated to v15.02.04
    Changes include:
    
    - New cert tests
    - UnsteadyAero.f90
        - Updated eqn numbers
        - added preprocessor flag: TEST_THEORY to evaluate modifications to algorithm based on theory document
    
    git-svn-id: https://windsvn2.nrel.gov/WT_Perf/branches/v4.x@222 a6739609-9c9d-4a29-a9ec-bdd8708b3f03

---
+++ CertTest/Run_Tests.bat
@@ -0,0 +1,26 @@
+..\bin\AeroDyn_Driver_Win32.exe AOC_Test01.dvr
+..\bin\AeroDyn_Driver_Win32.exe AOC_Test02.dvr
+..\bin\AeroDyn_Driver_Win32.exe AOC_Test03.dvr
+move AOC_Test01.1.out .\Results\AOC_Test01.1.out
+move AOC_Test01.2.out .\Results\AOC_Test01.2.out
+move AOC_Test01.3.out .\Results\AOC_Test01.3.out
+move AOC_Test02.1.out .\Results\AOC_Test02.1.out
+move AOC_Test02.2.out .\Results\AOC_Test02.2.out
+move AOC_Test02.3.out .\Results\AOC_Test02.3.out
+move AOC_Test03.1.out .\Results\AOC_Test03.1.out
+move AOC_Test03.2.out .\Results\AOC_Test03.2.out
+move AOC_Test03.3.out .\Results\AOC_Test03.3.out
+
+
+..\bin\AeroDyn_Driver_Win32.exe NRELOffshrBsline5MW_Onshore_Test01.dvr
+..\bin\AeroDyn_Driver_Win32.exe NRELOffshrBsline5MW_Onshore_Test02.dvr
+..\bin\AeroDyn_Driver_Win32.exe NRELOffshrBsline5MW_Onshore_Test03.dvr
+move NRELOffshrBsline5MW_Onshore_Test01.1.out .\Results\NRELOffshrBsline5MW_Onshore_Test01.1.out
+move NRELOffshrBsline5MW_Onshore_Test01.2.out .\Results\NRELOffshrBsline5MW_Onshore_Test01.2.out
+move NRELOffshrBsline5MW_Onshore_Test01.3.out .\Results\NRELOffshrBsline5MW_Onshore_Test01.3.out
+move NRELOffshrBsline5MW_Onshore_Test02.1.out .\Results\NRELOffshrBsline5MW_Onshore_Test02.1.out
+move NRELOffshrBsline5MW_Onshore_Test02.2.out .\Results\NRELOffshrBsline5MW_Onshore_Test02.2.out
+move NRELOffshrBsline5MW_Onshore_Test02.3.out .\Results\NRELOffshrBsline5MW_Onshore_Test02.3.out
+move NRELOffshrBsline5MW_Onshore_Test03.1.out .\Results\NRELOffshrBsline5MW_Onshore_Test03.1.out
+move NRELOffshrBsline5MW_Onshore_Test03.2.out .\Results\NRELOffshrBsline5MW_Onshore_Test03.2.out
+move NRELOffshrBsline5MW_Onshore_Test03.3.out .\Results\NRELOffshrBsline5MW_Onshore_Test03.3.out
